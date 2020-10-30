0187acedd90759f5347294854d2b5c33b7213cc6
Imran Ali <imranrali1@gmail.com>
2020-10-30 19:13:58 -0400

    repeating component and add if statement for if question is blank

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ function App() {
     <div className="App">
       <header className="App-header">
         Joke App
+      </header>
+      <div className="Joke">
         <JokeApp
           joke={{
             question:"",
-            joke:"because it did"}}/>
-      </header>
+            joke:"beat"}}/>
+        <JokeApp
+          joke={{
+            question:"The first time I realized",
+            joke:"I was living in this world"}}/>
+        <JokeApp
+          joke={{
+            question:"I was probably looking at the sky",
+            joke:"And forgive me but I dont know"}}/>
+        <JokeApp
+          joke={{
+            question:"If I remember what to say",
+            joke:"beat"}}/>
+        <JokeApp
+          joke={{
+            question:"",
+            joke:"What the air was really like that day"}}/>
+      </div>
     </div>
   );
 }
