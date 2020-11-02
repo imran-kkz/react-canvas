f89aec2111a3f4aea86d93db45328783a8267acf
Imran Ali <imranrali1@gmail.com>
2020-11-01 21:43:51 -0500

    adding product data

---
+++ src/productData.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: 1,
+        name: "Pencil",
+        price: 4,
+        description: "perfect for those remembering things"
+    },
+    {
+        id: 2,
+        name: "Crayon",
+        price: 3,
+        description: "Color away!"
+    },
+    {
+        id: 3,
+        name: "Eraser",
+        price: 2,
+        description: "Erase things!"
+    },
+    {
+        id: 4,
+        name: "Mug",
+        price: 1,
+        description: "new des"
+    },
+    {
+        id: 5,
+        name: "Pen",
+        price: 7,
+        description: "Big Boy over here!"
+    },
+]
