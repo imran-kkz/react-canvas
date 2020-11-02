3a557b28ceb329b97d707c1fdcc6d657dbe30ce8
Imran Ali <imranrali1@gmail.com>
2020-11-01 23:02:08 -0500

    setting up for new app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import jokesData from './jokesData';
 
 function App() {
-  const jokesComponents = jokesData.map(joke => <JokeApp key={joke.id} question={joke.question} answer={joke.answer}/>)
+  // const productsList = pro
   return (
-    <div className="App">
-      <header className="App-header">App</header>
-      {jokesComponents}
-    </div>
+  <div>
+
+  </div>
   )
 }
 
