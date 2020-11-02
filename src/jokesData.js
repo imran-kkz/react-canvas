a398b2a7e41fe9ed414ffec229c2b08b86fcc3fa
Imran Ali <imranrali1@gmail.com>
2020-11-01 20:17:34 -0500

    adding jokes data

---
+++ src/jokesData.js
@@ -0,0 +1,27 @@
+const jokesData = [
+    {
+        id: 1,
+        question:"",
+        joke:"beat"
+    },
+    {
+        id: 2,
+        question:"The first time I realized",
+        joke:"I was living in this world"
+    },
+    {
+        id: 3,
+        question:"I was probably looking at the sky",
+        joke:"And forgive me but I dont know"
+    },
+    {
+        id: 4,
+        question:"If I remember what to say",
+        joke:"beat"
+    },
+    {
+        id: 5,
+        question:"",
+        joke:"What the air was really like that day"
+    }
+]
