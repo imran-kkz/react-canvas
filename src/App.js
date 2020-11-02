36b69bcd695988c168c004df0140f4b4934344ee
Imran Ali <imranrali1@gmail.com>
2020-11-01 21:38:23 -0500

    finishing up mapping section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import jokesData from './jokesData';
 function App() {
   const jokesComponents = jokesData.map(joke => <JokeApp key={joke.id} question={joke.question} answer={joke.answer}/>)
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">App</header>
       {jokesComponents}
     </div>
   )
