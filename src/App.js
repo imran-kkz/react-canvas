3380f256a125e2a03c36f5225cca1881f8f4469f
Imran Ali <imranrali1@gmail.com>
2020-11-01 20:27:06 -0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import JokeApp from './JokeApp';
 import './App.css';
+import './jokesData'
+import jokesData from './jokesData';
 
 function App() {
+  const jokesComponents = jokesData.map((joke)=> {
+    return (
+      <JokeApp question={joke.question} joke={joke.joke}/>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        Joke App
-      </header>
-      <div className="Joke">
-        <JokeApp
-          joke={{
-            question:"",
-            joke:"beat"}}/>
-        <JokeApp
-          joke={{
-            question:"The first time I realized",
-            joke:"I was living in this world"}}/>
-        <JokeApp
-          joke={{
-            question:"I was probably looking at the sky",
-            joke:"And forgive me but I dont know"}}/>
-        <JokeApp
-          joke={{
-            question:"If I remember what to say",
-            joke:"beat"}}/>
-        <JokeApp
-          joke={{
-            question:"",
-            joke:"What the air was really like that day"}}/>
-      </div>
+    <div>
+      {jokesComponents}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/jokesData.js
+++ src/jokesData.js
@@ -2,27 +2,27 @@ const jokesData = [
     {
         id: 1,
         question:"",
-        joke:"beat"
+        answer:"beat"
     },
     {
         id: 2,
         question:"The first time I realized",
-        joke:"I was living in this world"
+        answer:"I was living in this world"
     },
     {
         id: 3,
         question:"I was probably looking at the sky",
-        joke:"And forgive me but I dont know"
+        answer:"And forgive me but I dont know"
     },
     {
         id: 4,
         question:"If I remember what to say",
-        joke:"beat"
+        answer:"beat"
     },
     {
         id: 5,
         question:"",
-        joke:"What the air was really like that day"
+        answer:"What the air was really like that day"
     }
 ]
 
