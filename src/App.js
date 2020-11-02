25a2557658a69884108229a5d2cacfa7196bda2a
Imran Ali <imranrali1@gmail.com>
2020-11-01 23:32:09 -0500

    learning mapping components with a data list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import JokeApp from './JokeApp';
+// import JokeApp from './JokeApp';
 import './App.css';
-import jokesData from './jokesData';
+// import jokesData from './jokesData';
+import products from './productData';
+import Product from './Product'
 
 function App() {
-  // const productsList = pro
+  const productsList = products.map((product)=> <Product name={product.name} price={product.price} description={product.description}/>)
   return (
   <div>
-
+    {productsList}
   </div>
   )
 }
--- src/Product.js
+++ src/Product.js
@@ -1,1 +1,12 @@
 import React from 'react'
+
+function Product(props){
+    return(
+        <div>
+            <h2>Product Name: {props.name}</h2>
+            <h3>Product Price: {props.price}</h3>
+            <h4>Product Description: {props.description}</h4>
+        </div>
+    )
+}
+export default Product
