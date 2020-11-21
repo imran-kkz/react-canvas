f0b1914685365c9c09bd099824161e2ea03782c0
imran-kkz <imranrali1@gmail.com>
2020-11-21 13:26:35 -0500

    learning1

--- src/Condition.js
+++ src/Condition.js
@@ -2,17 +2,23 @@ import React from 'react'
 
 function Condition(props) {
     console.log(props.loggedIn)
-    if (props.loggedIn == true){
-        return(
-            <h1>loading...</h1>
-        )
-    } else {
-        return (
-            <div>
-                <h1>loggedIn</h1>
-            </div>
-        )
-    }
+
+    return (
+        <div>
+            {props.loggedIn == true ? <h1>loading...</h1> : <h1>loggedin</h1>}
+        </div>
+    )
+    // if (props.loggedIn == true){
+    //     return(
+    //         <h1>loading...</h1>
+    //     )
+    // } else {
+    //     return (
+    //         <div>
+    //             <h1>loggedIn</h1>
+    //         </div>
+    //     )
+    // }
 }
 
 export default Condition
